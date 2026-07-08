3bc149055270b181c5c65175dc83b34003369733
Daniel Murangiri <danielmurangiri104@gmail.com>
2026-07-08 18:03:51 +0300

    Complete Task 7.1

---
+++ main.js
@@ -0,0 +1,14 @@
+// Variable declarations
+let name = "Daniel";
+let age = 25;
+const birthYear = 1999;
+
+// Display values
+console.log(name);
+console.log(age);
+console.log(birthYear);
+
+// Display data types
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof birthYear);
