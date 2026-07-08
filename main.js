bffa5ef806359a2bef07574244ebaf44f0daed92
Daniel Murangiri <danielmurangiri104@gmail.com>
2026-07-08 18:08:52 +0300

    Complete Task 7.2 Exercise 1

--- main.js
+++ main.js
@@ -12,3 +12,12 @@ console.log(birthYear);
 console.log(typeof name);
 console.log(typeof age);
 console.log(typeof birthYear);
+// Task 7.2 - Number Operations
+let a = 10;
+let b = 3;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
